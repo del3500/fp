d4f3fce1c95cdf003030fd0b38d9b2a0a01a8241
del3500 <wendellsmesia@gmail.com>
2025-07-21 01:23:38 +0800

    add annotated for str validation

--- api/strval/strval.py
+++ api/strval/strval.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Query
 from typing import Annotated
-
+from pydantic import StringConstraints
 
 app = FastAPI()
 
@@ -9,7 +9,21 @@ app = FastAPI()
 async def read_items(q: Annotated[list[str] | None, Query()] = None):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
-        results.update({"q": q})
+        for i in q:
+            results.update({"q": q})
     return results
 
 
+# Declaring more metadata for OpenAPI and docs 
+# TODO: update query param of v2
+
+query = Annotated[str, StringConstraint(min_length=3, max_length=5)]
+
+@app.get("/v2/items/")
+async def read_items_v2(
+    q: Annotated[list[str] | None, Query(title="Query string", min_length=3)] = None,
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Test"}]}
+    if q:
+        results.update({"q": q})
+    return results
