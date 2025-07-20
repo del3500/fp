c39ae00b7121fe5db3e78ce29e81dd3ad0a5c5b7
del3500 <wendellsmesia@gmail.com>
2025-07-20 23:36:29 +0800

    annotated list str

---
+++ api/strval/strval.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Query
+from typing import Annotated
+
+
+app = FastAPI()
+
+
+@app.get("/items/")
+async def read_items(q: Annotated[list[str] | None, Query()] = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+
