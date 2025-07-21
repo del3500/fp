70fafdf4a6b55a25e69134c8e137570c268eb57d
del3500 <wendellsmesia@gmail.com>
2025-07-21 13:15:15 +0800

    added custom validator

--- api/strval/strval.py
+++ api/strval/strval.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI, Query
+import random
 from typing import Annotated
+
+from fastapi import FastAPI, Query
 from pydantic import StringConstraints
+from pydantic import AfterValidator
 
 app = FastAPI()
 
@@ -14,16 +17,40 @@ async def read_items(q: Annotated[list[str] | None, Query()] = None):
     return results
 
 
-# Declaring more metadata for OpenAPI and docs 
-# TODO: update query param of v2
+# Declaring more metadata for OpenAPI and docs
 
-query = Annotated[str, StringConstraint(min_length=3, max_length=5)]
+query = Annotated[str, StringConstraints(min_length=3, max_length=5)]
+
 
 @app.get("/v2/items/")
 async def read_items_v2(
-    q: Annotated[list[str] | None, Query(title="Query string", min_length=3)] = None,
+    q: Annotated[query | None, Query(title="Query string", deprecated=True)] = None,
 ):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Test"}]}
     if q:
         results.update({"q": q})
     return results
+
+
+# Customer Validator
+
+data = {
+    "isbn-123456789": "Samle Book",
+    "imdb-987654321": "C Progamming Language",
+    "isbn-123456num": "ASM Bare Metal Lang"
+}
+
+def check_valid_id(id: str) -> str:
+    if not id.startswith(("isbn-", "imdb-")):
+        raise ValueError("Invalid ID Format, it must start with 'isbn-' or 'imdb-'")
+    return id
+
+@app.get("/v3/items")
+async def read_items_v3(id: Annotated[str | None, AfterValidator(check_valid_id)] = None):
+    if id:
+        item = data.get(id)
+    else:
+        id, item = random.choice(list(data.items()))
+    return {"id": id, "name": item} 
+    
+
